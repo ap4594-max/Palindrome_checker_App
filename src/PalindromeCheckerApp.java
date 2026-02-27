f9b4db174843055a692bd3af43931f1cc7af0f71
ap4594-max <ap4594@srmist.edu.in>
2026-02-27 15:49:26 +0530

    UC3: String Comparison

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class PalindromeCheckerApp {
 
     public static Boolean PalindromeChecker(String str){
@@ -13,9 +14,27 @@ public class PalindromeCheckerApp {
         return true;
     }
 
+    public static Boolean PalindromeChecker2(String str){
+        String duplicateString = str;
+        String reverse =  "";
+        for(int i = str.length() -1 ;i>=0;i--){
+            reverse = reverse + str.charAt(i);
+        }
+        return duplicateString.equals(reverse);
+
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to the Palindrome Checker Management System");
         System.out.println("Version: 1.0");
         System.out.println("System initialized successfully");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter input text : ");
+        String str = sc.nextLine();
+
+        System.out.println(PalindromeChecker(str));
+        System.out.println(PalindromeChecker2(str));
+
     }
 }
