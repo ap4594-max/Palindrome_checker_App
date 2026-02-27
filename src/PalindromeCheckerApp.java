5ee5eb1c2f558d102fd2ec7510174a65ec27e256
ap4594-max <ap4594@srmist.edu.in>
2026-02-27 15:12:08 +0530

    UC2: Hardcoded Palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,18 @@
 public class PalindromeCheckerApp {
 
+    public static Boolean PalindromeChecker(String str){
+        int i = 0;
+        int j = str.length()-1;
+        while(i<j){
+            if(str.charAt(i) != str.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to the Palindrome Checker Management System");
         System.out.println("Version: 1.0");
